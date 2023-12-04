2ec0494dfa73eff69f687fb0ee73d3cac2ef425b
CharlesWGillespie <charlesgillespiew@gmail.com>
2023-12-04 18:31:27 -0500

    added function

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-// const api = require("./routes/notes");
 
 app.use(express.static("public"));
 app.listen(PORT, () => {
